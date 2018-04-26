3d8a67a4d9b63c4d4b7b5e63f2a97d852326ec60
Dani <dani.xvh@gmail.com>
2018-04-26 14:30:21 +0200

    punteros y malloc

--- puntero14.cpp
+++ puntero14.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+<<<<<<< HEAD
+void Carga(int *pe){
+    int i;
+
+    for(i=0; i<5; i++){
+        printf("valor: ");
+        scanf("%i", &*pe);
+        pe++;
+    }
+}
+
+void Imprimir(int *pe){
+    int i;
+
+    for(i=0; i<5; i++){
+        printf("%i\n", *pe);
+        pe++;
+    }
+=======
 int Largo(char *cadena){
 
     int cont=0;
@@ -11,16 +30,24 @@ int Largo(char *cadena){
     }
     
     return cont;
+>>>>>>> a8ff70784ee843bcb2b82f63dd35d379a1ad86c5
 }
 
 int main(){
 
+<<<<<<< HEAD
+    int matriz[5];
+
+    Carga(matriz);
+    Imprimir(matriz);
+=======
     char nombre[20];
 
     printf("nombre: ");
     fgets(nombre,10,stdin);
 
     printf("el nombre %s tiene %i caracteres\n", nombre, Largo(nombre));
+>>>>>>> a8ff70784ee843bcb2b82f63dd35d379a1ad86c5
 
     return EXIT_SUCCESS;
 }
--- puntero15.cpp
+++ puntero15.cpp
@@ -7,6 +7,31 @@ int main(){
     int tam;
     int i;
 
+<<<<<<< HEAD
+    printf("\telementos del vector: ");
+    scanf("%i", &tam);
+
+    pe=(int*)malloc(tam*sizeof(int));
+
+    if(pe==NULL)
+        printf("\tError al reservar memoria\n");
+    else
+        printf("\tMemoria reservada sin problemas\n");
+
+    for(i=0; i<tam; i++){
+        printf("elemento: ");
+        scanf("%i", &pe[i]);
+    }
+
+    printf("\tContenido\n");
+
+    for(i=0; i<tam; i++)
+        printf("%i\n", pe[i]);
+
+    free(pe);
+
+    return EXIT_SUCCESS;
+=======
     printf("elementos del vector: ");
     scanf("%i", &tam);
 
@@ -27,4 +52,5 @@ int main(){
     free(pe);
 
     return 0;
+>>>>>>> a8ff70784ee843bcb2b82f63dd35d379a1ad86c5
 }
